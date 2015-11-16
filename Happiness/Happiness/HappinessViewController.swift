36d07f6c6463896b91c3ba93d407304d41d9a079
Dana Gregg <danagregg@hotmail.com>
2015-11-15 19:06:25 -0800

    Happiness initialized
    Basic happy face

---
+++ Happiness/Happiness/HappinessViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Dana Gregg on 11/15/15.
+//  Copyright © 2015 Dana Gregg. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+
+}
